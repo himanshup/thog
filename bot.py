c39df2fe5ecf54dbeb3541cddf286031cc24bb41
himanshup <himanshup@csu.fullerton.edu>
2018-10-06 04:19:51 -0700

    add weather command

--- bot.py
+++ bot.py
@@ -1,26 +1,35 @@
-import discord
+from discord.ext.commands import Bot
+from discord import Game
+from uszipcode import SearchEngine
+import requests
+import asyncio
 import os
+import json
 
 TOKEN = os.environ['TOKEN']
+DARK_SKY_API_TOKEN = os.environ['DARK_SKY_API_TOKEN']
+BOT_PREFIX = ('?', '!')
 
-client = discord.Client()
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+client = Bot(command_prefix=BOT_PREFIX)
 
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('-----')
+    await client.change_presence(game=Game(name="with humans"))
+
+
+@client.command()
+async def weather(zipcode):
+    search = SearchEngine(simple_zipcode=True)
+    zipcode = search.by_zipcode(zipcode)
+    data = zipcode.to_dict()
+
+    url = "https://api.darksky.net/forecast/" + DARK_SKY_API_TOKEN + "/" + str(
+        data['lat']) + "," + str(data['lng'])
+    response = requests.get(url)
+    value = response.json()['currently']['temperature']
+    await client.say('Current temp in ' + data['post_office_city'] + ' is ' +
+                     str(value))
 
 
 client.run(TOKEN)
