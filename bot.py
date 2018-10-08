19d89f7fc48d42127d9419b1e3b67f95ad178f93
himanshup <himanshup@csu.fullerton.edu>
2018-10-07 19:00:41 -0700

    add daily weataher forecast

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import os
 import json
 
 TOKEN = os.environ['TOKEN']
-DARK_SKY_API_TOKEN = os.environ['DARK_SKY_API_TOKEN']
+DARK_SKY_KEY = os.environ['DARK_SKY_KEY']
 BOT_PREFIX = ('?', '!')
 
 client = Bot(command_prefix=BOT_PREFIX)
@@ -30,17 +30,26 @@ async def weather(zipcode):
     else:
         data = results.to_dict()
 
-        url = "https://api.darksky.net/forecast/" + DARK_SKY_API_TOKEN + "/" + str(
+        url = "https://api.darksky.net/forecast/" + DARK_SKY_KEY + "/" + str(
             data['lat']) + "," + str(data['lng'])
+
         response = requests.get(url)
         temp = response.json()['currently']['temperature']
         icon = response.json()['currently']['icon']
-
+        dailySummary = response.json()['daily']['summary']
+        daily = response.json()['daily']['data']
         embed = discord.Embed(
             title=data['post_office_city'],
-            description=str(int(round(temp))) + chr(176),
+            description='Currently: ' + str(int(round(temp))) + chr(176),
             colour=discord.Colour.blue()
         )
+
+        embed.add_field(name='Daily Forecast',
+                        value=dailySummary, inline=False)
+
+        for temp in daily:
+            embed.add_field(name=str(datetime.utcfromtimestamp(temp['time']).strftime('%A %m/%d')), value='High: ' + str(
+                int(round(temp['temperatureHigh']))) + chr(176) + ' \nLow: ' + str(int(round(temp['temperatureLow']))) + chr(176), inline=True)
 
         embed.set_thumbnail(
             url='https://darksky.net/images/weather-icons/' + icon + '.png')
